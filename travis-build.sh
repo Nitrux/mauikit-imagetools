aab9bfd43a4d1358182ac4c8fdaf2c6e98bb6eff
Uri Herrera <hello@uriherrera.com>
2021-11-09 22:46:00 -0600

    update pkg

--- travis-build.sh
+++ travis-build.sh
@@ -26,7 +26,7 @@ mk-build-deps -i -t "apt-get --yes" -r
 
 ### Clone repo.
 
-git clone --depth 1 --branch v2.0.1 https://invent.kde.org/maui/mauikit-imagetools.git
+git clone --depth 1 --branch v2.1 https://invent.kde.org/maui/mauikit-imagetools.git
 
 mv mauikit-imagetools/* .
 
