9a453dbee39ebcc5d4d4628a846848fadd3ea471
Uri Herrera <hello@uriherrera.com>
2025-02-28 20:48:51 -0600

    fix runtime dep

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -75,7 +75,7 @@ checkinstall -D -y \
 	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=mauikit-imagetools-git \
-	--requires="libc6,libexiv2-28,libkf6coreaddons6,libkf6i18n6,libkf6iconthemes6,libleptonica6,libopencv-core410,libqt6core6t64,libqt6gui6,libqt6positioning6,libqt6positioning6-plugins,libqt6positioningquick6,libqt6qml6,libqt6quick6,libqt6quickcontrols2-6,libqt6quickshapes6,libqt6sql6,libqt6svg6,libqt6svgwidgets6,mauikit \(\>= 4.0.1\),qml6-module-org-kde-kirigami,qml6-module-org-kde-kquickimageeditor,qml6-module-qtquick-controls,qml6-module-qtquick-shapes,qml6-module-qtquick3d-spatialaudio,tesseract-ocr" \
+	--requires="libc6,libexiv2-28,libkf6coreaddons6,libkf6i18n6,libkf6iconthemes6,libleptonica6,libopencv-core410,libqt6core6t64,libqt6gui6,libqt6positioning6,libqt6positioning6-plugins,libqt6positioningquick6,libqt6qml6,libqt6quick6,libqt6quickcontrols2-6,libqt6quickshapes6,libqt6sql6,libqt6svg6,libqt6svgwidgets6,mauikit-git \(\>= 4.0.1\),qml6-module-org-kde-kirigami,qml6-module-org-kde-kquickimageeditor,qml6-module-qtquick-controls,qml6-module-qtquick-shapes,qml6-module-qtquick3d-spatialaudio,tesseract-ocr" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
