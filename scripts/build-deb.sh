e3c18cd96e6a8024573e960413bff38250b4d84d
Uri Herrera <hello@uriherrera.com>
2026-05-12 18:18:45 -0600

    update clone path

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -63,7 +63,7 @@ checkinstall -D -y \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
 	--pkggroup=libs \
-	--pkgsource=mauikit-imagetools \
+	--pkgsource=mauikit-imagetools-src \
 	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=mauikit-imagetools \
